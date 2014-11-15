48dbcf43f127c872b533be633265e08569c84da7
Adriano Cássio da Silva <adricarr@gmail.com>
2014-11-14 21:50:17 -0300

    Correção da pulo
    O pulo agora estas usando o RayCast para verificar o contanto com o
    chão.

--- Assets/Scritps/Player/Control.cs
+++ Assets/Scritps/Player/Control.cs
@@ -7,13 +7,11 @@ public class Control : MonoBehaviour {
 	public float jumpForce = 120;
 	public bool autoControl = true;
 	private Rigidbody controller;
-	private bool isJumping;
 
 	// Use this for initialization
 	void Start () {
 		controller = GetComponent<Rigidbody>();
 		rigidbody.freezeRotation = true;
-		isJumping = false;
 	}
 
 
@@ -23,29 +21,13 @@ public class Control : MonoBehaviour {
 			Vector3 moveDirection = new Vector3(moveSpeed, 0, 0);
 			controller.MovePosition(controller.position + moveDirection * Time.deltaTime);
 		}
-		if (Input.GetButtonDown("Jump") && !isJumping){
+		if (Input.GetButtonDown("Jump") && !isJumping()){
 			controller.AddForce(Vector3.up * jumpForce * 10 * Time.deltaTime, ForceMode.VelocityChange);
 		}
 	}
 
-	void Update(){
-	}
 
-	void OnCollisionEnter(Collision collision) {
-		if (collision.collider.tag == "Ground") {
-			isJumping = false;
-		}
-	}
-
-	void OnCollisionExit(Collision collision) {
-		if (collision.collider.tag == "Ground") {
-			isJumping = true;
-		}
-	}
-
-	void OnCollisionStay(Collision collision) {
-		if (collision.collider.tag == "Ground") {
-			isJumping = false;
-		}
+	bool isJumping(){
+		return Physics.Raycast(new Ray(transform.position, -Vector3.up));
 	}
 }
