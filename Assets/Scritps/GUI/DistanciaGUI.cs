01de40fe2d2309519b0a87a1a52cc2e4072094b2
mastercoks <matheusbdo1@gmail.com>
2014-11-17 02:18:43 -0300

    Organização e sistema de record
    [+]Sistema de Record

--- Assets/Scritps/GUI/DistanciaGUI.cs
+++ Assets/Scritps/GUI/DistanciaGUI.cs
@@ -4,11 +4,17 @@ using System.Collections;
 public class DistanciaGUI : MonoBehaviour {
 	
 	private Distancimentro distancimentro;
+	private int Record;
 	void Start () {
 		distancimentro = GameObject.Find("Inicio").GetComponent<Distancimentro>();
+		Record = PlayerPrefs.GetInt("Record");
 	}
 
 	void Update () {
-		guiText.text = distancimentro.distancia + " m";
+		guiText.text = "Distancia Percorrida: " + distancimentro.distancia + " m";
+		guiText.text += "\nRecord: " + Record + " m";
+		if (Record < distancimentro.distancia) {
+			PlayerPrefs.SetInt("Record",distancimentro.distancia);
+		}
 	}
 }
